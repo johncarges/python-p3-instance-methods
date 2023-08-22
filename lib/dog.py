efbaec2741e90081f0bbd3926d99f2a7922395f8
John Carges <johncarges@gmail.com>
2023-08-22 12:01:01 -0400

    passes all tests

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,10 @@
 
 class Dog:
     # Class body goes here
-
+    def sit(self):
+        print("The dog is sitting.")
     #Instance method definition
+
+    def bark(self):
+        print("Woof!")
     pass
